8f0c3184d8ea350d66626b5d1dda4f29d277d576
Ankitkumar-tech <95867896+Ankitkumar-tech@users.noreply.github.com>
2022-01-03 17:57:27 +0530

    modify all files
    first file index navavar
    second file menu

---
+++ index.js
@@ -0,0 +1,18 @@
+ function m1(){
+
+
+    window.location.href="menu.html"
+ }
+
+
+ function s1(){
+
+
+    window.location.href="signup.html"
+ }
+
+ function login1(){
+
+
+    window.location.href="login.html"
+ }
